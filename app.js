1227a3add73c727c14e618cb72427a84398ac7fa
tamim-jr <internationalinf24@gmail.com>
2024-04-18 16:29:43 +0600

    pratics some besic js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("tamim mahmud is in here");
+// Here I declared varivale 
+myName = " Tamim Mahmud";
+age = 24;
+x= null; //null means here is a empty value
+u = undefined; //we hva eno idea about this value
+profession = " web Developer";
+// here i call variables
+console.log("My Name is"+ myName + ", My age is "+ age + " and I am web devevloper" + profession);
